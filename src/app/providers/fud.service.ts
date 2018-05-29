c9d668b921b2dce2d612f1ef68da4e74d03b57ac
Adam Kaczmarek <bja1011@gmail.com>
2018-05-30 00:13:56 +0200

    More styles.

--- src/app/providers/fud.service.ts
+++ src/app/providers/fud.service.ts
@@ -17,7 +17,7 @@ export class FudService {
 
   getFudById(fudId: number): Fud {
     return this.fuds.find((fud) => {
-      return fud.id == fudId;
+      return fud.id === fudId;
     });
   }
 }
@@ -31,35 +31,35 @@ const FUD_MOCKS: Fud[] = [
     description: 'Not true,Not trueNot trueNot trueNot trueNot trueNot trueNot trueNot trueNot trueNot trueNot trueNot true'
   },
   {
-    id: 1,
-    title: 'XRP is not a cryptocurrency',
+    id: 2,
+    title: 'XRP is not a real cryptocurrency',
+    photo: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
+    excerpt: 'XRP is centralized shitcoin and Ripple can freeze Your tokens',
+    description: 'Definition of a blockchain: A blockchain is a continuously growing list of records, called blocks, which are linked and secured using cryptography, on a distributed ledger. Each block typically contains a cryptographic hash of the previous block, a timestamp and transaction data. This all applies on XRP.'
+  },
+  {
+    id: 3,
+    title: 'Ripple can freeze your coins',
     photo: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
     excerpt: 'XRP is centralized shitcoin and Ripple can freeze Your tokens',
     description: 'Not true,Not trueNot trueNot trueNot trueNot trueNot trueNot trueNot trueNot trueNot trueNot trueNot true'
   },
   {
-    id: 1,
+    id: 4,
     title: 'XRP is centralized',
     photo: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
     excerpt: 'XRP is centralized shitcoin and Ripple can freeze Your tokens',
     description: 'Not true,Not trueNot trueNot trueNot trueNot trueNot trueNot trueNot trueNot trueNot trueNot trueNot true'
   },
   {
-    id: 1,
+    id: 5,
     title: 'XRP is centralized',
     photo: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
     excerpt: 'XRP is centralized shitcoin and Ripple can freeze Your tokens',
     description: 'Not true,Not trueNot trueNot trueNot trueNot trueNot trueNot trueNot trueNot trueNot trueNot trueNot true'
   },
   {
-    id: 1,
-    title: 'XRP is centralized',
-    photo: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
-    excerpt: 'XRP is centralized shitcoin and Ripple can freeze Your tokens',
-    description: 'Not true,Not trueNot trueNot trueNot trueNot trueNot trueNot trueNot trueNot trueNot trueNot trueNot true'
-  },
-  {
-    id: 1,
+    id: 6,
     title: 'XRP is centralized',
     photo: 'https://material.angular.io/assets/img/examples/shiba2.jpg',
     excerpt: 'XRP is centralized shitcoin and Ripple can freeze Your tokens',
